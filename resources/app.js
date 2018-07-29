a95c3d50377eb3a288ec5ad464c1f6ecafad5ac2
Daniel Korosi <daniel.korosi42@gmail.com>
2018-07-29 10:06:44 +0200

    speech detection start stop

--- resources/app.js
+++ resources/app.js
@@ -1,7 +1,32 @@
 'use strict';
 
-let dataDisplayed = document.querySelector('.dataDisplayed');
-let button = document.querySelector('.button');
+
+let dataDisplayed = document.querySelector('.transcript');
+let startNow = 'Start now';
+let stop = 'Stop';
+let recordButton = document.querySelector('.record-button');
+recordButton.innerHTML = startNow;
+let sourceLang = document.querySelector('#source-lang');
+let targetLang = document.querySelector('#target-lang');
+
+let languages = [
+    {
+        name: "English",
+        code: "EN"
+    },
+    {
+        name: "Afrikaans",
+        code: "AF"
+    },
+    {
+        name: "Czech",
+        code: "CS"
+    },
+    {
+        name: "Danish",
+        code: "DA"
+    }
+];
 
 function addData (response) {
     console.log(response.entities);
@@ -11,17 +36,71 @@ function addData (response) {
     dataDisplayed.appendChild(dataItem);
 }
 
-function getAudio(url) {
+
+function createLanguageOptions (selectTag) {
+    languages.forEach(el => {
+        let selectOption = document.createElement('option')
+        selectOption.innerHTML = el.name;
+        selectTag.appendChild(selectOption)
+    })
+}
+
+createLanguageOptions(sourceLang);
+createLanguageOptions(targetLang);
+
+function addTranscript (transcript) {
+    let dataItem = document.createElement('p')
+    dataItem.innerHTML = transcript;
+    dataDisplayed.appendChild(dataItem);
+}
+
+let message = {
+    recording: false
+}
+
+function setRecording() {
+    recordButton.innerHTML = recordButton.innerHTML === startNow ? stop : startNow;
     let request = new XMLHttpRequest();
-    request.open('GET', url, true);
+    request.open('POST', 'http://localhost:3000/record', true);
+    request.setRequestHeader('Content-Type', 'application/json')
+    message.recording = message.recording === false ? true : false;
+    request.send(JSON.stringify(message));
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            if (request.response !== 'recording') {
+                let transcript = JSON.parse(request.response).join(' ');
+                console.log(transcript);
+                addTranscript(transcript);
+            }
+        }
+    }
+}
+
+function getSynsets () {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://babelnet.io/v5/getSynsetIds?lemma=beatles&searchLang=EN&targetLang=DE&pos=NOUN&key=13d7839c-ef5d-40fd-aa70-71d21f06181f', true);
+    request.send();
     request.onreadystatechange = () => {
         if (request.readyState === 4 && request.status === 200) {
             let response = JSON.parse(request.response);
             console.log(response);
-            addData(response);
+            response.forEach(item => {
+                getInterpretation(item.id);
+            })
         }
     }
-    request.send();
 }
 
-button.addEventListener('click', getAudio('http://localhost:3000/audio'));
+function getInterpretation (id) {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://babelnet.io/v5/getSynset?id=' + id + '&targetLang=IT&targetLang=DE&key=13d7839c-ef5d-40fd-aa70-71d21f06181f', true);
+    request.send();
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            let response = JSON.parse(request.response);
+            console.log(response);
+        }
+    }
+}
+
+recordButton.addEventListener('click', setRecording);
--- server.js
+++ server.js
@@ -1,87 +1,131 @@
 'use strict';
 
 let express = require('express');
+const bodyParser = require('body-parser');
 let app = express();
+
+app.use(bodyParser.json());
 app.use('/resources', express.static('resources'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
 const fs = require('fs');
 const language = require('@google-cloud/language');
+const record = require('node-record-lpcm16');
+const speech = require('@google-cloud/speech');
+const client = new speech.SpeechClient();
 
-// Creates a client
-const speechClient = new speech.SpeechClient();
-
-// The name of the audio file to transcribe
-const fileName = './resources/male_voice_cut.wav';
-
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
-
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 8000,
-    languageCode: 'en-US'
-};
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
 
 const request = {
-  audio: {
-    content: audioBytes,
-  },
-  config: config
+    config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+    },
+    interimResults: false, // If you want interim results, set this to true
 };
 
-// Creates a client
-const languageClient = new language.LanguageServiceClient();
+let isRecordStarted = false;
+let audioTranscript = [];
 
-let entityResponse = {
-    audioDetected: '',
-    entities: []
+
+function recordStream () {
+    const recognizeStream = client
+    .streamingRecognize(request)
+    .on('error', console.error)
+    .on('data', data => {
+        let fragment = data.results[0] && data.results[0].alternatives[0] ? data.results[0].alternatives[0].transcript : null;
+        console.log(fragment);
+        audioTranscript.push(fragment);
+    }
+    );
+    record
+        .start({
+        sampleRateHertz: sampleRateHertz,
+        threshold: 0,
+        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+        verbose: false,
+        recordProgram: 'rec', // Try also "arecord" or "sox"
+        silence: '10.0',
+        })
+        .on('error', console.error)
+        .pipe(recognizeStream);
 }
 
-app.get('/audio', function(req, res){
-    speechClient
-      .recognize(request)
-      .then(data => {
-        const response = data[0];
-        const transcription = response.results
-          .map(result => result.alternatives[0].transcript)
-          .join('\n');
-          response.audioDetected = transcription;
-          const documentToAnalyze = {
-            content: transcription,
-            type: 'PLAIN_TEXT',
-          };
-          languageClient
-            .analyzeEntities({document: documentToAnalyze})
-            .then(results => {
-              const entities = results[0].entities;
-              console.log('Entities:');
-              entities.forEach(entity => {
-                entityResponse.entities.push(entity);
-                console.log(entity);
-                console.log(entity.name);
-                // console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-                if (entity.metadata && entity.metadata.wikipedia_url) {
+function stopStream () {
+    record.stop();
+}
+
+app.post('/record', function(req, res){
+    console.log(req.body);
+    if (req.body.recording === false) {
+        console.log('stop');
+        stopStream();
+        res.send(audioTranscript);
+    } else {
+        console.log('start');
+        recordStream();
+        res.send('recording');
+    }
+});
+
+
+
+/* record
+  .start({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: 'rec', // Try also "arecord" or "sox"
+    silence: '10.0',
+  })
+  .on('error', console.error)
+  .pipe(recognizeStream);
+  console.log(recognizeStream);
+ app.get('/audio', function(req, res){
+    // Create a recognize stream
+    const recognizeStream = client
+    .streamingRecognize(request)
+    .on('error', console.error)
+    .on('data', data => {
+        let audioDetected = data.results[0] && data.results[0].alternatives[0] ? data.results[0].alternatives[0].transcript : null;
+        console.log(audioDetected);
+    });
+    // Start recording and send the microphone input to the Speech API
+    record
+      .start({
+        sampleRateHertz: sampleRateHertz,
+        threshold: 0,
+        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+        verbose: false,
+        recordProgram: 'arecord', // Try also "arecord" or "sox"
+        silence: '10.0',
+      })
+      .on('error', console.error)
+      .pipe(recognizeStream);
+      console.log(recognizeStream);
+      languageClient.analyzeEntities({document: documentToAnalyze}).then(results => {
+          const entities = results[0].entities;
+          console.log('Entities:');
+          console.log(entities);
+          entities.forEach(entity => {
+              entityResponse.entities.push(entity);
+              if (entity.metadata && entity.metadata.wikipedia_url) {
                   // console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
-                }
-              });
-              res.send(entityResponse);
-            })
-            .catch(err => {
-              console.error('ERROR:', err);
-            });
+              }
+          });
+          res.send(entities);
       })
       .catch(err => {
-        console.error('ERROR:', err);
+          console.error('ERROR:', err);
       });
-});
+}) */
 
 app.listen(3000, function(){
     console.log('server is running');
